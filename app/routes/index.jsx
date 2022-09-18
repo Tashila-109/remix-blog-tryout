3d0b241b22b7f32c9aa6dec6fc598f5a778a579f
Tashila <tashila.dev@gmail.com>
2022-09-18 17:34:55 +0400

    Fixed db client

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Welcome to Remix!</h1>
+      <h1>Welcome to Remix Blog!</h1>
       <p>
         Remix is a full stack web framework by the creators of React Router.
         This is a simple blog app from the Traversy Media Remix crash course.
